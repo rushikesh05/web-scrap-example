319ed2e9b06413b50d6d398bf6c3a47f1589caa5
rushikesh05 <rushikeshsurywanshi007@gmail.com>
2023-03-05 11:58:55 +0530

    web scrapper

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def index():
             logging.info("log my final result {}".format(reviews))
 
             
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://rushikesh007:rushikesh007@cluster0.vbmybtn.mongodb.net/?retryWrites=true&w=majority")
             db =client['scrapper_eng_pwskills']
             coll_pw_eng = db['scraper_pwskills_eng']
             coll_pw_eng.insert_many(reviews)
